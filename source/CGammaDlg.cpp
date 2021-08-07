656d91045b32b3ece3fcbb9670b5573c9d96bf63
leedohyeong <60651691+leedohyeong@users.noreply.github.com>
2021-08-07 15:15:40 +0900

    Add files via upload

---
+++ source/CGammaDlg.cpp
@@ -0,0 +1,37 @@
+﻿// CGammaDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "ImageProcessing.h"
+#include "CGammaDlg.h"
+#include "afxdialogex.h"
+
+
+// CGammaDlg 대화 상자
+
+IMPLEMENT_DYNAMIC(CGammaDlg, CDialog)
+
+CGammaDlg::CGammaDlg(CWnd* pParent /*=nullptr*/)
+	: CDialog(IDD_DIALOG6, pParent)
+	, m_GammaTrans(0)
+{
+
+}
+
+CGammaDlg::~CGammaDlg()
+{
+}
+
+void CGammaDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT1, m_GammaTrans);
+	DDV_MinMaxFloat(pDX, m_GammaTrans, 0, 2);
+}
+
+
+BEGIN_MESSAGE_MAP(CGammaDlg, CDialog)
+END_MESSAGE_MAP()
+
+
+// CGammaDlg 메시지 처리기
---
+++ source/CQuantization.cpp
@@ -0,0 +1,37 @@
+﻿// CQuantization.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "ImageProcessing.h"
+#include "CQuantization.h"
+#include "afxdialogex.h"
+
+
+// CQuantization 대화 상자
+
+IMPLEMENT_DYNAMIC(CQuantization, CDialog)
+
+CQuantization::CQuantization(CWnd* pParent /*=nullptr*/)
+	: CDialog(IDD_DIALOG3, pParent)
+	, m_QuantBit(0)
+{
+
+}
+
+CQuantization::~CQuantization()
+{
+}
+
+void CQuantization::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT1, m_QuantBit);
+	DDV_MinMaxInt(pDX, m_QuantBit, 1, 8);
+}
+
+
+BEGIN_MESSAGE_MAP(CQuantization, CDialog)
+END_MESSAGE_MAP()
+
+
+// CQuantization 메시지 처리기
